d878b8499790417f13e14a66cb4b912e6bab62bb
Belikhun <belivipro9x99@gmail.com>
2020-04-23 22:47:44 +0700

    3.5

---
+++ 3.5/extension/background.js
@@ -0,0 +1,35 @@
+console.log("Hello World! From Deal Hunter Suite Background Script!");
+
+chrome.browserAction.onClicked.addListener(tab => {
+    var currentTab = tab;
+
+    chrome.windows.create({
+        url: chrome.extension.getURL("/mainPage/index.html"),
+        width: 420,
+        height: 720,
+        type: "panel",
+        setSelfAsOpener: true
+    });
+});
+
+chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+    console.debug(details);
+
+    let headers = {}
+
+    for (let item of details.requestHeaders) {
+        let h = /FH_(.+)/gm.exec(item.name);
+
+        if (h && h[1]) {
+            console.log(`Processing Headers: ${item.name} (${h[1]}) -> ${item.value}`);
+            headers[h[1]] = item.value
+        } else
+            if (!headers[item.name])
+                headers[item.name] = item.value;
+    }
+
+    let headersArray = Object.keys(headers).map(key => ({ name: key, value: headers[key] }));
+    console.debug(headersArray);
+    
+    return { requestHeaders: headersArray }
+}, { urls: ["<all_urls>"] }, ["blocking", "extraHeaders", "requestHeaders"])
